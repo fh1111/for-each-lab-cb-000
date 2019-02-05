4e718793495203bbefd1551102417cc88137d24a
Fer Hernandez <fh.1111@gmail.com>
2019-02-05 03:01:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ function iterativeLog(array){
   array.forEach((el, i) => {
     console.log(`${i}: ${el}`)
   })
-  
+
+}
+
+
+function iterate(callback){
+  const cars = ["Ferrari", "Mercedez", "BMW"]
+  cars.forEach(callback)
+
 }
